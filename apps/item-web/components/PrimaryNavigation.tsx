6caa507943b72f4f7c3e228430ce2e49fab3a32c
injammed <absummers717@gmail.com>
2026-08-06 13:50:03 -0400

    Add persistent primary navigation

---
+++ apps/item-web/components/PrimaryNavigation.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { AccountGate } from "@/components/AccountGate";
+import { UploadGate } from "@/components/UploadGate";
+
+export function PrimaryNavigation() {
+  return (
+    <nav className="primary-navigation" aria-label="Primary navigation">
+      <a className="primary-navigation-link" href="#field">
+        <span className="primary-navigation-mark" aria-hidden="true">≋</span>
+        <span>Feed</span>
+      </a>
+
+      <div className="primary-navigation-action primary-navigation-submit">
+        <UploadGate />
+      </div>
+
+      <Link className="primary-navigation-link primary-navigation-museum" href="/aetimm/">
+        <span className="primary-navigation-mark" aria-hidden="true">◇</span>
+        <span>Museum</span>
+      </Link>
+
+      <Link className="primary-navigation-link" href="/about/">
+        <span className="primary-navigation-mark" aria-hidden="true">i</span>
+        <span>About</span>
+      </Link>
+
+      <div className="primary-navigation-action primary-navigation-account">
+        <AccountGate />
+      </div>
+    </nav>
+  );
+}
